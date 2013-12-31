2b1e52281fa02b289b7f82d16ca71d54ba677ef0
rengas <mr.rengaswamy@gmail.com>
2013-12-31 23:27:18 +0800

    Updated year

--- application/views/templates/year.php
+++ application/views/templates/year.php
@@ -10,7 +10,7 @@
 <?php //loop for every for cells
 	for($cell=0;$cell<5;$cell++)
 		 { 	if($item==$numberofyear){break;} ?>
-		 <td><a href= "<?php echo site_url("year/choosen/".$movies[$item]['Released'] ) ;?> "> 
+		 <td><a href= "<?php echo site_url("viewers/year/choosen/".$movies[$item]['Released'] ) ;?> "> 
 		     <?php echo $movies[$item]['Released'].'('.$movies[$item]['counter'].')'; $item++; } }?>
 		     </a>
 	     </td>
